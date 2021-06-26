077f2cd1e1583255467094fce883fa69ac2abb94
Shubham <shubham.sportypi@gmail.com>
2021-06-26 15:10:45 +0530

    contact 3

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -2,7 +2,7 @@
 * PHP Email Form Validation - v3.1
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
-*/
+
 (function () {
   "use strict";
 
@@ -82,4 +82,4 @@
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
-})();
+})();*/
